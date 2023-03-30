461fd6826aa93f4ac116fb0db7057ca1592e08a5
Aureliano <aureliano.franca@hotmail.com>
2023-03-30 17:29:46 -0300

    Installer.

---
+++ file/installer.go
@@ -0,0 +1,103 @@
+package file
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var osExecutable = os.Executable
+
+func Install(srcDir string) error {
+	dir, err := homeDir()
+	if err != nil {
+		return err
+	}
+
+	files, err := os.ReadDir(srcDir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), "tar.gz") || strings.HasSuffix(file.Name(), "zip") || file.Name() == "checksums.txt" {
+			continue
+		}
+
+		src := filepath.Join(srcDir, file.Name())
+		dest := filepath.Join(dir, file.Name())
+
+		fmt.Printf("Copy %s to %s\n", src, dest)
+		err = installFile(dest, src)
+		if err != nil {
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func homeDir() (string, error) {
+	ex, err := osExecutable()
+
+	if err != nil {
+		return "", fmt.Errorf("erro ao obter binário em execução: %w", err)
+	}
+
+	fi, err := os.Lstat(ex)
+	if err != nil {
+		return "", fmt.Errorf("erro ao obter informacões sobre o binário: %w", err)
+	}
+
+	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
+		path, err := filepath.EvalSymlinks(ex)
+		if err != nil {
+			return "", err
+		}
+
+		return filepath.Dir(path), nil
+	}
+
+	return filepath.Dir(ex), nil
+}
+
+func installFile(dest, src string) error {
+	destInfo, err := os.Stat(dest)
+	fileExist := true
+	var fm fs.FileMode
+
+	if os.IsNotExist(err) {
+		fileExist = false
+		fm = fs.FileMode(0644)
+	} else if err != nil {
+		return err
+	}
+
+	if fileExist {
+		if err = os.Remove(dest); err != nil {
+			return err
+		}
+		fm = fs.FileMode(destInfo.Mode())
+	}
+
+	out, err := os.OpenFile(dest, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, fm)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ file/installer_test.go
@@ -0,0 +1,215 @@
+package file
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInstall(t *testing.T) {
+	idir := filepath.Join(os.TempDir(), "14-bis", "test-install")
+	os.MkdirAll(idir, fs.ModePerm)
+
+	bin := filepath.Join(idir, "qtbis")
+	readme := filepath.Join(idir, "README.md")
+
+	file, err := os.Create(bin)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.WriteString("binary")
+	file.Close()
+
+	file, err = os.Create(readme)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.WriteString("read-me")
+	file.Close()
+
+	file, err = os.Create(filepath.Join(idir, "14-bis_Linux_x86_64.tar.gz"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	file.Close()
+
+	file, err = os.Create(filepath.Join(idir, "14-bis_Linux_x86_64.zip"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	file.Close()
+
+	target, err := homeDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = Install(idir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file, err = os.Open(filepath.Join(target, "qtbis"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer file.Close()
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := string(bytes)
+	expected := "binary"
+	assert.Equal(t, expected, actual)
+
+	file, err = os.Open(filepath.Join(target, "README.md"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer file.Close()
+	bytes, err = io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual = string(bytes)
+	expected = "read-me"
+	assert.Equal(t, expected, actual)
+
+	_, err = os.Stat("14-bis_Linux_x86_64.tar.gz")
+	assert.NotNil(t, err, err)
+
+	_, err = os.Stat("14-bis_Linux_x86_64.zip")
+	assert.NotNil(t, err, err)
+
+	os.RemoveAll(idir)
+}
+
+func TestHomeDirOsExecutableFail(t *testing.T) {
+	osExecutable = func() (string, error) { return "", fmt.Errorf("some error") }
+	_, err := homeDir()
+
+	actual := err.Error()
+	expected := "erro ao obter binário em execução: some error"
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestHomeDirUnknownPath(t *testing.T) {
+	osExecutable = func() (string, error) { return "/unknown/path", nil }
+	_, err := homeDir()
+
+	actual := err.Error()
+	expected := "erro ao obter informacões sobre o binário: lstat /unknown/path: no such file or directory"
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestHomeDir(t *testing.T) {
+	osExecutable = os.Executable
+	dir, err := homeDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	parts := strings.Split(dir, string(filepath.Separator))
+	if len(parts) < 3 {
+		t.Fatal("Expected at least 2 tokens.")
+	}
+
+	actual := parts[1]
+	expected := filepath.Base(os.TempDir())
+	assert.Equal(t, expected, actual)
+
+	actual = parts[2]
+	expected = "go-build"
+	assert.Contains(t, actual, expected)
+}
+
+func TestInstallFileNew(t *testing.T) {
+	file, err := os.Create(filepath.Join(os.TempDir(), "file.txt"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.WriteString("12345")
+	file.Close()
+
+	dest := filepath.Join(os.TempDir(), "install-new.txt")
+	err = installFile(dest, file.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file, err = os.Open(dest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := string(bytes)
+	expected := "12345"
+
+	assert.Equal(t, expected, actual)
+
+	os.Remove(filepath.Join(os.TempDir(), "file.txt"))
+	os.Remove(dest)
+}
+
+func TestInstallFileReplace(t *testing.T) {
+	file, err := os.Create(filepath.Join(os.TempDir(), "file.txt"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.WriteString("54321")
+	file.Close()
+	source := file.Name()
+
+	dest := filepath.Join(os.TempDir(), "install-replace.txt")
+	file, err = os.Create(dest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.WriteString("12345")
+	file.Close()
+
+	err = installFile(dest, source)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file, err = os.Open(dest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := string(bytes)
+	expected := "54321"
+
+	assert.Equal(t, expected, actual)
+
+	os.Remove(filepath.Join(os.TempDir(), "file.txt"))
+	os.Remove(dest)
+}
